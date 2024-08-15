d91e80c156e22bfcfec0a658d356fddc59fb5f7c
Caio Balduino <caiobalduinodev@hotmail.com>
2024-08-15 15:57:05 -0300

    finish Contact page

---
+++ src/Components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className={styles.header}>
+      <ul>
+        <li>
+          <NavLink className={styles.link} to="/" end>
+            Produtos
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={styles.link} to="contato">
+            Contato
+          </NavLink>
+        </li>
+      </ul>
+      <h1>Header</h1>
+    </nav>
+  );
+};
+
+export default Header;
